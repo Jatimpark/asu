cc3c2b75bbf6b53e1aa7ae65a76e9e5ece40fb8b
Jatimpark <133945139+Jatimpark@users.noreply.github.com>
2023-07-19 15:36:23 +0700

    Add files via upload

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,67 +1,47 @@
 #!/bin/bash
-# ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
-# Getting
-clear
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+###########- COLOR CODE -##############
+NC="\e[0m"
+RED="\033[0;31m" 
+
+
 IP=$(wget -qO- ipinfo.io/ip);
 date=$(date +"%Y-%m-%d")
 clear
-email=$(cat /home/email)
-if [[ "$email" = "" ]]; then
-echo "Masukkan Email Untuk Menerima Backup"
-read -rp "Email : " -e email
-cat <<EOF>>/home/email
-$email
-EOF
-fi
-clear
-echo "Mohon Menunggu , Proses Backup sedang berlangsung !!"
-rm -rf /root/backup
+echo "Starting Backup"
+sleep 1
+echo "Membuat Directory"
 mkdir /root/backup
-cp -r /root/.acme.sh /root/backup/ &> /dev/null
-cp /etc/passwd /root/backup/ &> /dev/null
-cp /etc/group /root/backup/ &> /dev/null
-cp /etc/shadow /root/backup/ &> /dev/null
-cp /etc/gshadow /root/backup/ &> /dev/null
-cp /etc/ppp/chap-secrets /root/backup/chap-secrets &> /dev/null
-cp /etc/ipsec.d/passwd /root/backup/passwd1 &> /dev/null
-cp -r /var/lib/SIJA/ /root/backup/SIJA &> /dev/null
-cp -r /etc/xray /root/backup/xray &> /dev/null
-cp -r /home/vps/public_html /root/backup/public_html &> /dev/null
+sleep 1
+echo "Start Backup"
+sleep 2
+clear
+cp /etc/passwd backup/
+cp /etc/group backup/
+cp /etc/shadow backup/
+cp /etc/gshadow backup/
+cp -r /var/lib/SIJA/ backup/SIJA
+cp -r /etc/xray backup/xray
+cp -r /home/vps/public_html backup/public_html
+cp -r /root/udp backup/udp
 cd /root
-zip -r $IP-$date.zip backup > /dev/null 2>&1
-rclone copy /root/$IP-$date.zip dr:backup/
-url=$(rclone link dr:backup/$IP-$date.zip)
+zip -r $Name-$IP-$date.zip backup > /dev/null 2>&1
+rclone copy /root/$Name-$IP-$date.zip dr:
+url=$(rclone link dr:$Name-$IP-$date.zip)
 id=(`echo $url | grep '^https' | cut -d'=' -f2`)
 link="https://drive.google.com/u/4/uc?id=${id}&export=download"
-echo -e "
-Detail Backup Arya Blitar
-==================================
-IP VPS        : $IP
-Link Backup   : $link
-Tanggal       : $date
-==================================
-" | mail -s "Backup Data" $email
+echo -e "The following is a link to your vps data backup file."
+echo -e "=================================" 
+echo -e "Detail Backup : "
+echo -e "================================="
+echo -e "IP VPS        : $IP"
+echo -e "Client Name   : $Name"
+echo -e "Link Backup   : $link"
+echo -e "================================="
+echo -e "Script By Arya Blitar"
+echo ""
+echo -e "If you want to restore data, please enter the link above"
 rm -rf /root/backup
-rm -r /root/$IP-$date.zip
-clear
-echo -e "
-Detail Backup Arya Blitar
-==================================
-IP VPS        : $IP
-Link Backup   : $link
-Tanggal       : $date
-==================================
-"
-echo
-read -n 1 -s -r -p "   Press any key to back on menu"
-menu
+rm -r /root/$Name-$IP-$date.zip
+echo "Done"
