01a9aa01da456557c3b8a6fbcd8ea204e91c0911
Jatimpark <133945139+Jatimpark@users.noreply.github.com>
2023-08-31 05:53:07 +0700

    Add files via upload

--- trgo/addtrgo.sh
+++ trgo/addtrgo.sh
@@ -88,10 +88,10 @@ until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
 		fi
 	done
 read -p "Expired (Days) : " masaaktif
-sed -i '/"'""$uuid""'"$/a\,"'""$user""'"' /etc/trojan-go/config.json
+sed -i '/#trojango$/a\#### '"$user /etc/trojan-go/config.json
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 hariini=`date -d "0 days" +"%Y-%m-%d"`
-echo -e "## $user $exp" >> /etc/trojan-go/akun.conf
+echo -e "#### $user $exp" >> /etc/trojan-go/akun.conf
 systemctl restart trojan-go.service
 link="trojan://${user}@bug.com:2087/?sni=${domain}&type=ws&host=${domain}&path=/trojango&encryption=none#$user"
 clear
